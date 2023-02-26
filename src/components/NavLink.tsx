43dea50dc33d35ef910b8797f9e3186f1ee8b67e
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-02-26 17:59:01 +0630

    feat: make NavLink knows relative routes

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,4 +1,10 @@
-import { Link, LinkProps, matchRoutes, useLocation } from 'react-router-dom'
+import {
+  Link,
+  LinkProps,
+  matchRoutes,
+  useLocation,
+  useResolvedPath,
+} from 'react-router-dom'
 
 import { routes } from '../App'
 
@@ -15,21 +21,23 @@ const NavLink = ({
   activeClassName,
   inactiveClassName,
   exact,
+  ...rest
 }: Props) => {
   const location = useLocation()
   const matchedRoutes = matchRoutes(routes, location)
+  const resolvedPath = useResolvedPath(to)
 
   let isActive
   if (exact) {
-    isActive = location.pathname === to
+    isActive = location.pathname === resolvedPath.pathname
   } else {
-    isActive = matchedRoutes?.some((route) => route.pathname === to)
+    isActive = matchedRoutes?.some((route) => route.pathname === resolvedPath.pathname)
   }
 
   const allClassName = `${className} ${isActive ? activeClassName : inactiveClassName}`
 
   return (
-    <Link to={to} className={allClassName}>
+    <Link to={to} className={allClassName} {...rest}>
       {children}
     </Link>
   )
